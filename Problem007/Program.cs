6c944bd1c2be7cbd6b1792a94fdcf2cde6ccc85b
Scott Pedersen <spederse@gmail.com>
2011-05-31 23:59:50 -0700

    Finished problem 7

--- Problem007/Program.cs
+++ Problem007/Program.cs
@@ -9,7 +9,7 @@ namespace Problem007
     /// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can 
     /// see that the 6th prime is 13.
     ///
-    /// What is the 10001st prime number?    
+    /// What is the 10001st prime number?
     /// </summary>
     class Program
     {
@@ -30,23 +30,14 @@ namespace Problem007
 
         private static int SolveProblem()
         {
-            // 104743
-            var primes = new List<int> { 2, 3, 5, 7, 11, 13 };
-            const int primorial = 30030;
-            var potential = Enumerable.Range(1, primorial - 1).Where(x => primes.TrueForAll(y => x%y != 0)).ToList();
-
-            for (var k = 0;; k++)
+            var primes = new List<int> {2, 3, 5, 7, 11, 13};
+            for (var i = 15; primes.Count < 10001; i += 2)
             {
-                for (var i = 0; i < potential.Count; i++)
-                {
-                    var prime = primorial*k + potential[i];
-                    if (prime == 1)
-                        continue;
-                    primes.Add(prime);
-                    if (primes.Count() >= 10001)
-                        return primes[10000];
-                }
+                var i1 = i;
+                if(primes.TrueForAll(x => i1%x != 0))
+                    primes.Add(i1);
             }
+            return primes.Max();
         }
     }
 }
